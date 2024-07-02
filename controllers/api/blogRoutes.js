c17b68fe98af8129d8cadeb8ae58510af8e5c8c2
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-07-02 16:30:11 -0400

    styled homepage.css and fixed blog content boxes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-// Import the Project model from the models folder
+// Import the blog model from the models folder
 const { Blog } = require('../../models');
 
-// If a POST request is made to /api/projects, a new project is created. If there is an error, the function returns with a 400 error. 
+// If a POST request is made to /api/blog, a new blog is created. If there is an error, the function returns with a 400 error. 
 router.post('/', async (req, res) => {
   console.log(req.body)
   try {
@@ -20,22 +20,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-// If a DELETE request is made to /api/projects/:id, that project is deleted. 
+// If a DELETE request is made to /api/blog/:id, that blog is deleted. 
 router.delete('/:id', async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const blogData = await Blog.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!blogData) {
+      res.status(404).json({ message: 'No blogfound with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(blogData);
   } catch (err) {
     res.status(500).json(err);
   }
